a81a161c94aa8b43c8f932ce489ffb22596c0d40
Bryan Batchelder <bbatchelder@gmail.com>
2018-10-16 17:31:41 -0400

    Added some more comments

--- libzt.Interop.DemoServer/Program.cs
+++ libzt.Interop.DemoServer/Program.cs
@@ -39,25 +39,18 @@ namespace libzt.Interop.DemoServer
                 GCHandle sockaddr_storage_handle = GCHandle.Alloc(sockaddr_storage_buffer, GCHandleType.Pinned);
                 IntPtr addressStoragePtr = GCHandle.ToIntPtr(sockaddr_storage_handle);
 
-                if ((err = libzt.zts_get_address(nwid, addressStoragePtr, (int)libzt.SockAddrFamily.Inet)) != 0)
+                if ((err = libzt.zts_get_address(nwid, addressStoragePtr, (int)libzt.SockAddrFamily.Inet)) == 0)
                 {
-                    throw new Exception("Error calling zts_get_address.");
-                }
-
-                libzt.SOCKADDR_STORAGE addressStorage = Marshal.PtrToStructure<libzt.SOCKADDR_STORAGE>(addressStoragePtr);
-
-                if (err == 0)
-                {
+                    libzt.SOCKADDR_STORAGE addressStorage = Marshal.PtrToStructure<libzt.SOCKADDR_STORAGE>(addressStoragePtr);
                     var ipbytes = BitConverter.GetBytes(addressStorage.Data2[0]);
                     var ip = new IPAddress(ipbytes);
                     Console.WriteLine("ZeroTier address [{0}]", ip);
+                    sockaddr_storage_handle.Free();
                 }
                 else
                 {
-                    throw new Exception("Problem getting address.");
+                    throw new Exception("Error calling zts_get_address.");
                 }
-
-                sockaddr_storage_handle.Free();
 
                 int fd = -1;
 
@@ -91,6 +84,9 @@ namespace libzt.Interop.DemoServer
                 }
                 Console.WriteLine("Done.");
 
+                //Free unmanaged memory
+                Marshal.FreeHGlobal(addrPtr);
+
                 Console.Write("Trying to put socket into listening state...");
                 if ((err = libzt.zts_listen(fd, 3)) < 0)
                 {
@@ -98,20 +94,33 @@ namespace libzt.Interop.DemoServer
                 }
                 Console.WriteLine("Done.");
 
+                //Number of file descriptors to check when calling select().  Since fds are zero-based this should
+                //be the highest value fd + 1.
                 int nfds = 0;
+
+                //Timeval struct to hold our timeout value when calling select().  Select() will return at the first sign of activity
+                //or when the timeout elapses, whichever comes first.  Timeval holds both seconds and microseconds.
                 var tv = new libzt.TIMEVAL();
-                var readfds = new libzt.FDSET();
-                var writefds = new libzt.FDSET();
-                var exceptds = new libzt.FDSET();
-                var handlerfds = new libzt.FDSET();
-
-                byte[] messageBuffer = new byte[64];
-
                 int msecs = 250;
                 tv.tv_sec = msecs / 1000;
                 tv.tv_usec = (msecs % 1000) * 1000;
 
+                //File Descriptor Sets that we'll use when calling select.  In this example we really only care about the
+                //read state, so that is the only set we populate.  The others are passed in empty.
+                var readfds = new libzt.FDSET();
+                var writefds = new libzt.FDSET();
+                var exceptfds = new libzt.FDSET();
+
+                //This FD set is used to track active client connections.  We'll use this to iterate through client sockets
+                //and check if they are ready to be read from, and remove them from the set when the connection is closed.
+                var handlerfds = new libzt.FDSET();
+
+                //The message buffer used for receiving data.
+                byte[] messageBuffer = new byte[64];
+
                 int fdret = -1;
+
+                //Zero-out the list of handler FDs.
                 libzt.FD_ZERO(ref handlerfds);
 
                 while (true)
@@ -119,28 +128,32 @@ namespace libzt.Interop.DemoServer
                     if (Console.KeyAvailable)
                         break;
 
-                    nfds = 0;
-
+                    //Clear all the fd sets
                     libzt.FD_ZERO(ref readfds);
                     libzt.FD_ZERO(ref writefds);
-                    libzt.FD_ZERO(ref exceptds);
+                    libzt.FD_ZERO(ref exceptfds);
 
+                    //Add the main listener socket to readfds
                     libzt.FD_SET(fd, ref readfds);
 
+                    //Reset nfds on each loop.  Important when client sockets start closing, so we don't poll
+                    //dead sockets.
+                    nfds = 1;
+
+                    //Populate readfds with all our client sockets
                     for (int i=0; i<32; i++)
                     {
                         if (libzt.FD_ISSET(i, ref handlerfds) > 0)
                         {
                             libzt.FD_SET(i, ref readfds);
 
-                            if (nfds < i)
-                                nfds = i;
+                            //Remember nfds must be the largest value fd + 1
+                            if (nfds < (i + 1))
+                                nfds = (i + 1);
                         }
-                    }
+                    }  
 
-                    nfds += 1; //TODO: need to verify this is actually needed.  
-
-                    int ret = libzt.zts_select(nfds, ref readfds, ref writefds, ref exceptds, ref tv);
+                    int ret = libzt.zts_select(nfds, ref readfds, ref writefds, ref exceptfds, ref tv);
 
                     if (ret == 0)
                         continue;
@@ -202,8 +215,6 @@ namespace libzt.Interop.DemoServer
                 libzt.zts_close(fd);
                 //Stop ZeroTier
                 libzt.zts_stop();
-
-                Marshal.FreeHGlobal(addrPtr);
             }
             catch (Exception ex)
             {
